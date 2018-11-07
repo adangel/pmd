ee95bed27e52a33a9f73aba1bc178cec16e3abec
Clément Fournier <clement.fournier76@gmail.com>
2018-11-07 08:52:19 +0100

    Add TODOs

--- pmd-core/src/main/java/net/sourceforge/pmd/lang/ast/xpath/AttributeAxisIterator.java
+++ pmd-core/src/main/java/net/sourceforge/pmd/lang/ast/xpath/AttributeAxisIterator.java
@@ -7,6 +7,7 @@ package net.sourceforge.pmd.lang.ast.xpath;
 import java.lang.reflect.Method;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collection;
 import java.util.HashSet;
 import java.util.Iterator;
 import java.util.List;
@@ -122,6 +123,9 @@ public class AttributeAxisIterator implements Iterator<Attribute> {
     }
 
 
+    class doo<F extends Collection<T>, T> {
+
+    }
     /**
      * Associates an attribute accessor with the XPath-accessible
      * name of the attribute. This is used to avoid recomputing
