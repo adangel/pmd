131834de711389f36bb15e4f4ee0f0285d11742a
Andreas Dangel <andreas.dangel@adangel.org>
2019-02-13 20:13:34 +0100

    [plsql] Support ListaggOverflowClause for LISTAGG function

--- pmd-plsql/src/main/java/net/sourceforge/pmd/lang/plsql/ast/PLSQLParserVisitorAdapter.java
+++ pmd-plsql/src/main/java/net/sourceforge/pmd/lang/plsql/ast/PLSQLParserVisitorAdapter.java
@@ -1011,4 +1011,9 @@ public class PLSQLParserVisitorAdapter implements PLSQLParserVisitor {
     public Object visit(ASTWithinClause node, Object data) {
         return visit((PLSQLNode) node, data);
     }
+
+    @Override
+    public Object visit(ASTListaggOverflowClause node, Object data) {
+        return visit((PLSQLNode) node, data);
+    }
 }
